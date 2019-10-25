f071442fdbaf431ebe19b95fc80a1e961f8e4113
LidiaLu <Lidija.Luneva@accenture.com>
2019-10-25 14:24:24 +0300

    adds some explanation of code

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -4,10 +4,15 @@ import { bookReducer } from '../reducers/bookReducer';
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
+
+  //const [state, dispatch] = useReducer(reducer, initialArg, init);  reducer returms two elements aas array -state and dispatch function
+  //init :  It’s an optional value that will lazily create the initial state. That means we can calculate the initial state/value with an init function outside of the reducer instead of providing an explicit value. 
+  //That’s handy if the initial value could be different, say based on a last saved state instead of a consistent value.
   const [books, dispatch] = useReducer(bookReducer, [], () => {
     const localData = localStorage.getItem('books');
     return localData ? JSON.parse(localData) : [];
   });
+
   useEffect(() => {
     localStorage.setItem('books', JSON.stringify(books));
   }, [books]);
